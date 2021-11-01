25da765a8445b500891839ebe4d3d1ac91668ca8
YukiIgarashi <yuki.igarashi@rakus-partners.co.jp>
2021-11-01 15:16:29 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleYuki2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleYuki2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleYuki2Application.class, args);
+	}
+
+}
